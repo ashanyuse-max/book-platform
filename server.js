1c761b7f0321163d913a05a5e44eb294b75d6ae7
ashanyuse-max <ashanyuse@gmail.com>
2026-03-25 04:38:23 +0530

    Modern UI Overhaul: Glassmorphism, Responsive UI, and Animated Books Grids

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const path = require("path");
+require("dotenv").config();
 
 const app = express();
 
